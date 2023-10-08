09a33d6dce3faf2475932b713516512df5d119c9
Lucas Correia <lucas.correia@me.com>
2023-10-08 20:30:01 -0300

    adiciona scripts tibia

---
+++ lr.py
@@ -0,0 +1,12 @@
+import pyautogui
+import pygetwindow as gw
+import time
+
+while(True):
+    # win = gw.getWindowsWithTitle('Tibia - Criminal Didi')[0]
+    # win.activate()
+    time.sleep(2)
+    pyautogui.press('l')
+    time.sleep(1200)
+    
+    
---
+++ sd.py
@@ -0,0 +1,14 @@
+import pyautogui
+import pygetwindow as gw
+import time
+
+while(True):
+    # win = gw.getWindowsWithTitle('Tibia - Criminal Didi')[0]
+    # win.activate()
+    time.sleep(2)
+    pyautogui.press('p')
+    pyautogui.press('p')
+    pyautogui.press('o')
+    time.sleep(110)
+    
+    
